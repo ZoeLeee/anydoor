c9832374bc1740b5717ab3d14be627d06fb2fe3a
Zoe <lizhou1021@qq.com>
2018-02-28 22:41:19 +0800

    learn

--- src/app.js
+++ src/app.js
@@ -3,23 +3,12 @@ const chalk=require("chalk");
 const conf=require("./config/defaultConfig");
 const path=require("path");
 const fs=require("fs");
+const route=require("./helper/route");
+
 
 const server =http.createServer((req,res)=>{
   const filePath=path.join(conf.root,req.url);
-  fs.stat(filePath,(err,stats)=>{
-    if(err){
-      res.statusCode=404;
-      res.setHeader('Content-Type',"text/plain");
-      res.end(`${filePath} is not a directory or no exist`);
-      return
-    }
-    if(stats.isFile()){
-      
-    }
-  })
-  res.statusCode=200;
-  res.setHeader('Content-Type',"text/html");
-  res.end(filePath);
+  route(req,res,filePath);
 })
 server.listen(conf.port,conf.hostname,()=>{
   const addr=`http://${conf.hostname}:${conf.port}`;
