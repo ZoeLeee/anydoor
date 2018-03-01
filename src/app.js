5e61599b6907010c9b3ed1ea8f54a295b4e97347
Zoe <lizhou1021@qq.com>
2018-03-01 22:13:24 +0800

    learn mime type

--- src/app.js
+++ src/app.js
@@ -5,7 +5,6 @@ const path=require("path");
 const fs=require("fs");
 const route=require("./helper/route");
 
-
 const server =http.createServer((req,res)=>{
   const filePath=path.join(conf.root,req.url);
   route(req,res,filePath);
--- src/helper/route.js
+++ src/helper/route.js
@@ -2,19 +2,33 @@ const fs=require("fs");
 const promisify=require("util").promisify;
 const stat=promisify(fs.stat);
 const readdir=promisify(fs.readdir);
+const path=require("path")
+const config=require("../config/defaultConfig")
+const HandleBars=require("handlebars");
+const mime=require('./mime')
+
+const source=fs.readFileSync(path.join(__dirname,'../template/dir.tpl'));
+const template=HandleBars.compile(source.toString())
 
 module.exports=async function (req,res,filePath) {
   try{
     const stats=await stat(filePath);
     if(stats.isFile()){
+      const contentType=mime(filePath)
       res.statusCode=200;
-      res.setHeader('Content-Type',"text/plain");
+      res.setHeader('Content-Type',contentType);
       fs.createReadStream(filePath).pipe(res)
     }else if(stats.isDirectory){
       const files=await readdir(filePath);
+      const dir=path.relative(config.root,filePath);
       res.statusCode=200;
-      res.setHeader('Content-Type',"text/plain");
-      res.end(files.join(","));
+      res.setHeader('Content-Type',"text/html");
+      const data={
+        title:path.basename(filePath),
+        files,
+        dir:dir?`/${dir}`:''
+      };
+      res.end(template(data));
     }
   }catch(ex){
     res.statusCode=404;
