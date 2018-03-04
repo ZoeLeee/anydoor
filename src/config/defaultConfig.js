c4272a68a7df484fe17e7979c3736f5afe922365
Zoe <33168975+ZoeLeee@users.noreply.github.com>
2018-03-04 16:40:13 +0800

    压缩，缓存

--- src/config/defaultConfig.js
+++ src/config/defaultConfig.js
@@ -1,5 +1,13 @@
 module.exports={
   root:process.cwd(),//当前路径
   hostname:"127.0.0.1",
-  port:9527
+  port:9527,
+  compress:/\.(html|js|css|md)/,
+  cache:{
+    maxAge:600,
+    expires:true,
+    cacheControl:true,
+    lastModified:true,
+    etag:true
+  }
 }
--- src/helper/route.js
+++ src/helper/route.js
@@ -6,18 +6,39 @@ const path=require("path")
 const config=require("../config/defaultConfig")
 const HandleBars=require("handlebars");
 const mime=require('./mime')
+const compress=require('./compress')
 
 const source=fs.readFileSync(path.join(__dirname,'../template/dir.tpl'));
 const template=HandleBars.compile(source.toString())
+const range=require('./range')
+
+const isFresh=require('./cache');
 
 module.exports=async function (req,res,filePath) {
   try{
     const stats=await stat(filePath);
     if(stats.isFile()){
       const contentType=mime(filePath)
-      res.statusCode=200;
       res.setHeader('Content-Type',contentType);
-      fs.createReadStream(filePath).pipe(res)
+
+      if(isFresh(stats,req,res)){
+        res.statusCode=304;
+        res.end();
+        return;
+      }
+
+      let rs;
+      const {code,start,end}=range(stats.size,req,res);
+      res.statusCode=code;
+      if(code===200){
+        rs=fs.createReadStream(filePath)
+      }else{
+        rs=fs.createReadStream(filePath,{start,end})
+      }
+      if(filePath.match(config.compress)){
+        rs=compress(rs,req,res);
+      }
+      rs.pipe(res);
     }else if(stats.isDirectory){
       const files=await readdir(filePath);
       const dir=path.relative(config.root,filePath);
